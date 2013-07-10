7db040c78c3ac2a90188d93c41c15a76e4a3ead9
Tim G <santr.des@gmail.com>
2013-07-09 23:34:44 -0400

    Outputting content of index.html now

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
 
 var app = express.createServer(express.logger());
 
+var fileBuf = fs.readFileSync('index.html');
+var fileContent = fileBuf.toString();
+
 app.get('/', function(request, response) {
-  response.send('Hello World 2!');
+//  response.send('Hello World 2!');
+    response.send(fileContent);
 });
 
 var port = process.env.PORT || 5000;
