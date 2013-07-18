f7f88834dcf70c670b3b7c09cad31234c254544b
Tim Gilyazetdinov <tgilyazetdinov@amplify.com>
2013-07-18 14:11:00 -0400

    Added server.js script and added npm connect

---
+++ server.js
@@ -0,0 +1,8 @@
+var connect = require('connect');
+var port = process.env.PORT || 5000;
+
+connect.createServer(
+    connect.static(__dirname)
+).listen(port);
+
+console.log("Server running on port :" + port);
